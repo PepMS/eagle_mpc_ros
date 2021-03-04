ed5fcbbbcfb0116082520eaf9cfdcc5625955ca3
PepMS <jmarti@iri.upc.edu>
2021-03-04 12:09:35 +0100

    MpcRunner: working with Carrot & Iris

--- multicopter_mpc_controller/include/mpc_runner.hpp
+++ multicopter_mpc_controller/include/mpc_runner.hpp
@@ -16,18 +16,32 @@
 #include "multicopter_mpc_msgs/SolverPerformance.h"
 
 #include "crocoddyl/core/utils/timer.hpp"
+#include "crocoddyl/core/solvers/box-fddp.hpp"
 
 #include "multicopter_mpc_controller/ParamsConfig.h"
-#include "multicopter_mpc/mpc-main.hpp"
+
+#include "multicopter_mpc/trajectory.hpp"
+#include "multicopter_mpc/mpc-controllers/carrot-mpc.hpp"
+#include "multicopter_mpc/mpc-base.hpp"
 
 class MpcRunner {
  public:
   MpcRunner();
   ~MpcRunner();
 
-  void runMpcStep();
-
  private:
+  void initializeParameters();
+  void initializeMpcController();
+  void initializeVariables();
+  void initializeMsgs();
+  void initializeSubscribers();
+  void initializePublishers();
+
+  void callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odometry);
+  void callbackOdometryGains(const nav_msgs::OdometryConstPtr &msg_odometry);
+  void callbackMpcSolve(const ros::TimerEvent &);
+  void callbackConfig(multicopter_mpc_controller::ParamsConfig &config, uint32_t level);
+
   // ROS objects & methods
   ros::NodeHandle nh_;
 
@@ -42,18 +56,35 @@ class MpcRunner {
   dynamic_reconfigure::Server<multicopter_mpc_controller::ParamsConfig> server_;
   dynamic_reconfigure::Server<multicopter_mpc_controller::ParamsConfig>::CallbackType callback_server_;
 
-  mav_msgs::Actuators msg_actuators_;
+  // Msgs
+  mav_msgs::Actuators msg_thrusts_;
   multicopter_mpc_msgs::WholeBodyState msg_whole_body_state_;
   multicopter_mpc_msgs::SolverPerformance msg_solver_performance_;
+  multicopter_mpc_msgs::MotorsState msg_motors_state_;
+  std::mutex mut_motors_state_;
 
-  void callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odometry);
-  void callbackOdometryGains(const nav_msgs::OdometryConstPtr &msg_odometry);
-  void callbackMpcSolve(const ros::TimerEvent &);
-  void callbackConfig(multicopter_mpc_controller::ParamsConfig &config, uint32_t level);
+  // Mpc Related
+  boost::shared_ptr<multicopter_mpc::Trajectory> trajectory_;
+  boost::shared_ptr<multicopter_mpc::CarrotMpc> carrot_mpc_;
+
+  // Variables & Parameters
+  struct NodeParams {
+    std::string trajectory_config_path;
+    std::size_t trajectory_dt;
+    multicopter_mpc::SolverTypes trajectory_solver;
+    std::string trajectory_integration;
+    bool trajectory_squash;
+
+    std::string mpc_config_path;
+    std::string mpc_type;
+
+    bool use_internal_gains;
+    bool record_solver;
+    std::size_t motor_command_dt;
+  } node_params_;
 
   // MPC related
   double motor_command_dt_;
-  bool record_solver_;
 
   // state
   std::mutex mut_state_;
@@ -61,27 +92,21 @@ class MpcRunner {
   Eigen::VectorXd state_ref_;
   Eigen::VectorXd state_diff_;
   ros::Time state_time_;
-  ros::Time state_used_time_;
-  boost::shared_ptr<crocoddyl::StateMultibody> robot_state_;
 
   // thrust command
   std::mutex mut_control_;
-  Eigen::VectorXd motors_thrust_;
-  Eigen::VectorXd motors_thrust_new_;
-  Eigen::VectorXd motors_speed_;
+  Eigen::VectorXd control_command_;
+  Eigen::VectorXd thrust_command_;
+  // Eigen::VectorXd motors_thrust_new_;
+  Eigen::VectorXd speed_command_;
+  Eigen::VectorXd torque_command_;
   ros::Time control_last_;
 
   // gains
   std::mutex mut_gains_;
   bool flag_new_gains_;
-  Eigen::VectorXd ff_gains_;
   Eigen::MatrixXd fb_gains_;
-  Eigen::MatrixXd fb_gains_new_;
-
-  std::mutex mut_motors_state_;
-  multicopter_mpc_msgs::MotorsState msg_motors_state_;
-
-  multicopter_mpc::MpcMain mpc_main_;
+  // Eigen::MatrixXd fb_gains_new_;
 
   // Solver
   ros::WallTime solver_time_init_;
@@ -89,14 +114,10 @@ class MpcRunner {
 
   // Controller state machine
   bool controller_started_;
+  ros::Time controller_start_time_;
+  ros::Duration controller_time_;
+  std::size_t controller_instant_;
 
-  // Visualization related
-  visualization_msgs::Marker trajectory_points_;
-
-  void fillTrajectoryMsg();
-
-  // Debug
-  bool printed_;
 };
 
 // #endif
--- multicopter_mpc_controller/src/mpc_runner.cpp
+++ multicopter_mpc_controller/src/mpc_runner.cpp
@@ -1,73 +1,126 @@
 #include "mpc_runner.hpp"
 
+#include "multicopter_mpc/utils/tools.hpp"
+
 MpcRunner::MpcRunner() {
-  std::string mission_path;
-  std::string mpc_controller_type;
-  std::string mpc_main_yaml_path;
-  bool use_internal_gains;
+  initializeParameters();
+  initializeMpcController();
+  initializeVariables();
+  initializeMsgs();
+  initializeSubscribers();
 
-  // Parameters
-  nh_.param<std::string>(ros::this_node::getNamespace() + "/mission_yaml_path", mission_path, "");
-  nh_.param<std::string>(ros::this_node::getNamespace() + "/mpc_main_yaml_path", mpc_main_yaml_path, "");
-  nh_.param<bool>(ros::this_node::getNamespace() + "/use_internal_gains", use_internal_gains, "false");
-  nh_.param<bool>(ros::this_node::getNamespace() + "/record_solver", record_solver_, "false");
+  controller_started_ = false;
+  initializePublishers();
+  // RQT Config
+  callback_server_ = boost::bind(&MpcRunner::callbackConfig, this, _1, _2);
+  server_.setCallback(callback_server_);
+}
 
-  // Mpc Controller
-  mpc_main_ = multicopter_mpc::MpcMain(multicopter_mpc::MultiCopterTypes::Iris, mission_path, mpc_main_yaml_path);
+MpcRunner::~MpcRunner() {}
 
-  if (use_internal_gains) {
-    nh_.param<double>(ros::this_node::getNamespace() + "/motor_command_dt", motor_command_dt_, 0.01);
+void MpcRunner::initializeParameters() {
+  // Trajectory related
+  nh_.param<std::string>(ros::this_node::getNamespace() + "/trajectory_config", node_params_.trajectory_config_path,
+                         "");
+  int trajectory_dt;
+  nh_.param<int>(ros::this_node::getNamespace() + "/trajectory_dt", trajectory_dt, 10);
+  node_params_.trajectory_dt = (std::size_t)trajectory_dt;
+
+  std::string trajectory_solver;
+  nh_.param<std::string>(ros::this_node::getNamespace() + "/trajectory_solver", trajectory_solver, "SolverSbFDDP");
+  node_params_.trajectory_solver = multicopter_mpc::SolverTypes_map.at(trajectory_solver);
+
+  nh_.param<std::string>(ros::this_node::getNamespace() + "/trajectory_integration",
+                         node_params_.trajectory_integration, "IntegratedActionModelEuler");
+  node_params_.trajectory_squash = node_params_.trajectory_solver == multicopter_mpc::SolverTypes::SolverSbFDDP;
+
+  nh_.param<std::string>(ros::this_node::getNamespace() + "/mpc_config", node_params_.mpc_config_path, "");
+  nh_.param<bool>(ros::this_node::getNamespace() + "/use_internal_gains", node_params_.use_internal_gains, false);
+  nh_.param<bool>(ros::this_node::getNamespace() + "/record_solver", node_params_.record_solver, false);
+
+  int motor_command_dt;
+  nh_.param<int>(ros::this_node::getNamespace() + "/motor_command_dt", motor_command_dt, 0);
+  node_params_.motor_command_dt = (std::size_t)motor_command_dt;
+}
+
+void MpcRunner::initializeMpcController() {
+  trajectory_ = multicopter_mpc::Trajectory::create();
+  trajectory_->autoSetup(node_params_.trajectory_config_path);
+
+  ROS_WARN("This is the dt: %ld", node_params_.trajectory_dt);
+  boost::shared_ptr<crocoddyl::ShootingProblem> problem = trajectory_->createProblem(
+      node_params_.trajectory_dt, node_params_.trajectory_squash, node_params_.trajectory_integration);
+
+  boost::shared_ptr<crocoddyl::SolverFDDP> solver;
+  switch (node_params_.trajectory_solver) {
+    case multicopter_mpc::SolverTypes::SolverSbFDDP:
+      solver = boost::make_shared<multicopter_mpc::SolverSbFDDP>(problem, trajectory_->get_squash());
+      break;
+    case multicopter_mpc::SolverTypes::SolverBoxFDDP:
+      solver = boost::make_shared<crocoddyl::SolverBoxFDDP>(problem);
+      break;
+  }
+  std::vector<boost::shared_ptr<crocoddyl::CallbackAbstract>> callbacks;
+  callbacks.push_back(boost::make_shared<crocoddyl::CallbackVerbose>());
+  solver->setCallbacks(callbacks);
+  solver->solve(crocoddyl::DEFAULT_VECTOR, crocoddyl::DEFAULT_VECTOR);
+
+  carrot_mpc_ = boost::make_shared<multicopter_mpc::CarrotMpc>(
+      trajectory_, solver->get_xs(), node_params_.trajectory_dt, node_params_.mpc_config_path);
+}
+
+void MpcRunner::initializeVariables() {
+  if (node_params_.use_internal_gains) {
+    motor_command_dt_ = node_params_.motor_command_dt / 1000.0;
   } else {
-    motor_command_dt_ = mpc_main_.getMpcController()->getTimeStep();
+    motor_command_dt_ = carrot_mpc_->get_dt() / 1000.0;
   }
 
   // States
-  state_ = mpc_main_.getMpcController()->getStateMultibody()->zero();
+  state_ = carrot_mpc_->get_robot_state()->zero();
   state_ref_ = state_;
-  state_diff_ = Eigen::VectorXd::Zero(mpc_main_.getMpcController()->getStateMultibody()->get_ndx());
+  state_diff_ = Eigen::VectorXd::Zero(carrot_mpc_->get_robot_state()->get_ndx());
 
   // Thrust command
-  motors_thrust_ = Eigen::VectorXd::Zero(mpc_main_.getMpcController()->getActuation()->get_nu());
-  motors_speed_ = motors_thrust_;
+  control_command_ = Eigen::VectorXd::Zero(carrot_mpc_->get_actuation()->get_nu());
+  thrust_command_ = Eigen::VectorXd::Zero(carrot_mpc_->get_platform_params()->n_rotors_);
+  speed_command_ = thrust_command_;
+  if (control_command_.size() - thrust_command_.size() > 0) {
+    torque_command_ = Eigen::VectorXd::Zero(control_command_.size() - thrust_command_.size());
+  }
 
   // Gains
   flag_new_gains_ = false;
-  fb_gains_ = Eigen::MatrixXd::Zero(mpc_main_.getMpcController()->getActuation()->get_nu(),
-                                    mpc_main_.getMpcController()->getStateMultibody()->get_ndx());
+  fb_gains_ = Eigen::MatrixXd::Zero(carrot_mpc_->get_actuation()->get_nu(), carrot_mpc_->get_robot_state()->get_ndx());
+}
 
-  msg_actuators_.angular_velocities.resize(motors_thrust_.size());
-  msg_motors_state_.thrusts.resize(motors_thrust_.size());
-  msg_whole_body_state_.thrusts.resize(motors_thrust_.size());
+void MpcRunner::initializeMsgs() {
+  msg_thrusts_.angular_velocities.resize(thrust_command_.size());
+  msg_motors_state_.thrusts.resize(thrust_command_.size());
+  msg_whole_body_state_.thrusts.resize(thrust_command_.size());
+}
 
-  controller_started_ = false;
-
+void MpcRunner::initializeSubscribers() {
   // Subscribers
-  if (use_internal_gains) {
+  if (node_params_.use_internal_gains) {
     subs_odom_ =
         nh_.subscribe("/odometry", 1, &MpcRunner::callbackOdometryGains, this, ros::TransportHints().tcpNoDelay());
-    timer_mpc_solve_ = nh_.createTimer(ros::Duration(mpc_main_.getMpcController()->getTimeStep()),
-                                       &MpcRunner::callbackMpcSolve, this);
+    timer_mpc_solve_ =
+        nh_.createTimer(ros::Duration(carrot_mpc_->get_dt() / 1000.0), &MpcRunner::callbackMpcSolve, this);
     ROS_INFO_STREAM("Publishing motor control every: " << motor_command_dt_ << " seconds");
   } else {
     subs_odom_ =
         nh_.subscribe("/odometry", 1, &MpcRunner::callbackOdometryMpc, this, ros::TransportHints().tcpNoDelay());
   }
+}
 
-  // Publishers
+void MpcRunner::initializePublishers() {
   pub_motor_command_ = nh_.advertise<mav_msgs::Actuators>("/motor_command", 1);
-  if (record_solver_) {
+  if (node_params_.record_solver) {
     pub_solver_performance_ = nh_.advertise<multicopter_mpc_msgs::SolverPerformance>("solver_performance", 1);
     pub_whole_body_state_ = nh_.advertise<multicopter_mpc_msgs::WholeBodyState>("whole_body_state", 1);
   }
-
-  // RQT Config
-  callback_server_ = boost::bind(&MpcRunner::callbackConfig, this, _1, _2);
-  server_.setCallback(callback_server_);
-
-  printed_ = false;
 }
-
-MpcRunner::~MpcRunner() {}
 
 void MpcRunner::callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odometry) {
   mut_state_.lock();
@@ -89,31 +142,41 @@ void MpcRunner::callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odomet
 
   if (controller_started_) {
     mut_state_.lock();
-    mpc_main_.setCurrentState(state_);
+    carrot_mpc_->get_problem()->set_x0(state_);
     mut_state_.unlock();
 
-    if (record_solver_) {
+    controller_time_ = ros::Time::now() - controller_start_time_;
+    controller_instant_ = (std::size_t)(controller_time_.toSec() * 1000.0);
+    carrot_mpc_->updateProblem(controller_instant_);
+
+    if (node_params_.record_solver) {
       solver_time_init_ = ros::WallTime::now();
     }
-    mpc_main_.runMpcStep(1);
-    motors_speed_ = mpc_main_.getMotorsSpeed();
-    if (ros::Duration(ros::Time::now() - control_last_).toSec() > 1.5 * mpc_main_.getMpcController()->getTimeStep()) {
+    carrot_mpc_->get_solver()->solve(carrot_mpc_->get_solver()->get_xs(), carrot_mpc_->get_solver()->get_us(),
+                                     carrot_mpc_->get_iters());
+
+    // PROPERLY HANDLE SOLVER TYPE!
+    control_command_ = boost::static_pointer_cast<multicopter_mpc::SolverSbFDDP>(carrot_mpc_->get_solver())->getSquashControls()[0];
+
+    thrust_command_ = control_command_.head(thrust_command_.size());
+    multicopter_mpc::Tools::thrustToSpeed(thrust_command_, carrot_mpc_->get_platform_params(), speed_command_);
+    if (ros::Duration(ros::Time::now() - control_last_).toSec() > 1.5 * carrot_mpc_->get_dt()) {
       ROS_WARN("Control rate not accomplished!");
     }
     control_last_ = ros::Time::now();
 
-    msg_actuators_.header.stamp = ros::Time::now();
-    msg_actuators_.angular_velocities[0] = motors_speed_(0);
-    msg_actuators_.angular_velocities[1] = motors_speed_(1);
-    msg_actuators_.angular_velocities[2] = motors_speed_(2);
-    msg_actuators_.angular_velocities[3] = motors_speed_(3);
-    pub_motor_command_.publish(msg_actuators_);
+    msg_thrusts_.header.stamp = ros::Time::now();
+    msg_thrusts_.angular_velocities[0] = speed_command_(0);
+    msg_thrusts_.angular_velocities[1] = speed_command_(1);
+    msg_thrusts_.angular_velocities[2] = speed_command_(2);
+    msg_thrusts_.angular_velocities[3] = speed_command_(3);
+    pub_motor_command_.publish(msg_thrusts_);
 
-    if (record_solver_) {
+    if (node_params_.record_solver) {
       solver_duration_ = ros::WallTime::now() - solver_time_init_;
       msg_solver_performance_.header.stamp = ros::Time::now();
-      msg_solver_performance_.final_cost = mpc_main_.getMpcController()->getSolver()->get_cost();
-      msg_solver_performance_.iters = mpc_main_.getMpcController()->getSolver()->get_iter();
+      msg_solver_performance_.final_cost = carrot_mpc_->get_solver()->get_cost();
+      msg_solver_performance_.iters = carrot_mpc_->get_solver()->get_iter();
       msg_solver_performance_.solving_time.sec = solver_duration_.sec;
       msg_solver_performance_.solving_time.nsec = solver_duration_.nsec;
       msg_solver_performance_.state_initial.pose = msg_odometry->pose.pose;
@@ -123,8 +186,8 @@ void MpcRunner::callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odomet
       msg_whole_body_state_.header.stamp = msg_odometry->header.stamp;
       msg_whole_body_state_.floating_base.pose = msg_odometry->pose.pose;
       msg_whole_body_state_.floating_base.motion = msg_odometry->twist.twist;
-      for (std::size_t i = 0; i < 4; ++i) {
-        msg_whole_body_state_.thrusts[i].speed_command = motors_speed_(i);
+      for (std::size_t i = 0; i < speed_command_.size(); ++i) {
+        msg_whole_body_state_.thrusts[i].speed_command = speed_command_(i);
       }
       pub_whole_body_state_.publish(msg_whole_body_state_);
     }
@@ -132,81 +195,84 @@ void MpcRunner::callbackOdometryMpc(const nav_msgs::OdometryConstPtr &msg_odomet
 }
 
 void MpcRunner::callbackOdometryGains(const nav_msgs::OdometryConstPtr &msg_odometry) {
-  mut_state_.lock();
-  state_(0) = msg_odometry->pose.pose.position.x;
-  state_(1) = msg_odometry->pose.pose.position.y;
-  state_(2) = msg_odometry->pose.pose.position.z;
-  state_(3) = msg_odometry->pose.pose.orientation.x;
-  state_(4) = msg_odometry->pose.pose.orientation.y;
-  state_(5) = msg_odometry->pose.pose.orientation.z;
-  state_(6) = msg_odometry->pose.pose.orientation.w;
-  state_(7) = msg_odometry->twist.twist.linear.x;
-  state_(8) = msg_odometry->twist.twist.linear.y;
-  state_(9) = msg_odometry->twist.twist.linear.z;
-  state_(10) = msg_odometry->twist.twist.angular.x;
-  state_(11) = msg_odometry->twist.twist.angular.y;
-  state_(12) = msg_odometry->twist.twist.angular.z;
-  state_time_ = msg_odometry->header.stamp;
-  mut_state_.unlock();
+  // mut_state_.lock();
+  // state_(0) = msg_odometry->pose.pose.position.x;
+  // state_(1) = msg_odometry->pose.pose.position.y;
+  // state_(2) = msg_odometry->pose.pose.position.z;
+  // state_(3) = msg_odometry->pose.pose.orientation.x;
+  // state_(4) = msg_odometry->pose.pose.orientation.y;
+  // state_(5) = msg_odometry->pose.pose.orientation.z;
+  // state_(6) = msg_odometry->pose.pose.orientation.w;
+  // state_(7) = msg_odometry->twist.twist.linear.x;
+  // state_(8) = msg_odometry->twist.twist.linear.y;
+  // state_(9) = msg_odometry->twist.twist.linear.z;
+  // state_(10) = msg_odometry->twist.twist.angular.x;
+  // state_(11) = msg_odometry->twist.twist.angular.y;
+  // state_(12) = msg_odometry->twist.twist.angular.z;
+  // state_time_ = msg_odometry->header.stamp;
+  // mut_state_.unlock();
 
-  if (controller_started_) {
-    if (flag_new_gains_) {
-      mut_gains_.lock();
-      motors_thrust_ = motors_thrust_new_;
-      fb_gains_ = fb_gains_new_;
-      flag_new_gains_ = false;
-      mut_gains_.unlock();
-    }
+  // if (controller_started_) {
+  //   if (flag_new_gains_) {
+  //     mut_gains_.lock();
+  //     motors_thrust_ = motors_thrust_new_;
+  //     fb_gains_ = fb_gains_new_;
+  //     flag_new_gains_ = false;
+  //     mut_gains_.unlock();
+  //   }
 
-    mut_state_.lock();
-    ros::Duration elapsed_time = ros::Time::now() - control_last_;
-    if (elapsed_time > ros::Duration(motor_command_dt_)) {
-      ROS_WARN("Control rate not accomplished!");
-    }
-    mpc_main_.getMpcController()->getStateMultibody()->diff(state_ref_, state_, state_diff_);
-    mut_state_.unlock();
+  //   mut_state_.lock();
+  //   ros::Duration elapsed_time = ros::Time::now() - control_last_;
+  //   if (elapsed_time > ros::Duration(motor_command_dt_)) {
+  //     ROS_WARN("Control rate not accomplished!");
+  //   }
+  //   mpc_main_.getMpcController()->getStateMultibody()->diff(state_ref_, state_, state_diff_);
+  //   mut_state_.unlock();
 
-    mut_control_.lock();
-    motors_thrust_ = motors_thrust_ - fb_gains_ * state_diff_;
-    motors_thrust_ = (motors_thrust_.array() < 0).select(0, motors_thrust_);  // Avoid negative numbers
-    mpc_main_.thrustToSpeed(motors_thrust_, motors_speed_);
-    mut_control_.unlock();
+  //   mut_control_.lock();
+  //   motors_thrust_ = motors_thrust_ - fb_gains_ * state_diff_;
+  //   motors_thrust_ = (motors_thrust_.array() < 0).select(0, motors_thrust_);  // Avoid negative numbers
+  //   mpc_main_.thrustToSpeed(motors_thrust_, motors_speed_);
+  //   mut_control_.unlock();
 
-    msg_actuators_.header.stamp = ros::Time::now();
-    msg_actuators_.angular_velocities[0] = motors_speed_(0);
-    msg_actuators_.angular_velocities[1] = motors_speed_(1);
-    msg_actuators_.angular_velocities[2] = motors_speed_(2);
-    msg_actuators_.angular_velocities[3] = motors_speed_(3);
-    pub_motor_command_.publish(msg_actuators_);
-  }
+  //   msg_actuators_.header.stamp = ros::Time::now();
+  //   msg_actuators_.angular_velocities[0] = motors_speed_(0);
+  //   msg_actuators_.angular_velocities[1] = motors_speed_(1);
+  //   msg_actuators_.angular_velocities[2] = motors_speed_(2);
+  //   msg_actuators_.angular_velocities[3] = motors_speed_(3);
+  //   pub_motor_command_.publish(msg_actuators_);
+  // }
 }
 
 void MpcRunner::callbackMpcSolve(const ros::TimerEvent &) {
-  if (controller_started_) {
-    mut_state_.lock();
-    ros::Duration elapsed_time = ros::Time::now() - state_time_;
-    if (elapsed_time > ros::Duration(0.01)) {
-      ROS_WARN("MPC Solve: Outdated state");
-    }
-    // Here we should account for the solving delay
-    mpc_main_.setCurrentState(state_);
-    state_ref_ = state_;
-    mut_state_.unlock();
+  // if (controller_started_) {
+  //   mut_state_.lock();
+  //   ros::Duration elapsed_time = ros::Time::now() - state_time_;
+  //   if (elapsed_time > ros::Duration(0.01)) {
+  //     ROS_WARN("MPC Solve: Outdated state");
+  //   }
+  //   // Here we should account for the solving delay
+  //   mpc_main_.setCurrentState(state_);
+  //   state_ref_ = state_;
+  //   mut_state_.unlock();
 
-    mpc_main_.runMpcStep();
+  //   mpc_main_.runMpcStep();
 
-    mut_gains_.lock();
-    control_last_ = ros::Time::now();
-    motors_thrust_new_ = mpc_main_.getMotorsThrust();
-    fb_gains_new_ = mpc_main_.getFeedBackGains();
-    flag_new_gains_ = true;
-    mut_gains_.unlock();
-  }
+  //   mut_gains_.lock();
+  //   control_last_ = ros::Time::now();
+  //   motors_thrust_new_ = mpc_main_.getMotorsThrust();
+  //   fb_gains_new_ = mpc_main_.getFeedBackGains();
+  //   flag_new_gains_ = true;
+  //   mut_gains_.unlock();
+  // }
 }
 
 void MpcRunner::callbackConfig(multicopter_mpc_controller::ParamsConfig &config, uint32_t level) {
   ROS_INFO("RECONFIGURE_REQUEST!");
   controller_started_ = config.start_mission;
+  if (controller_started_) {
+    controller_start_time_ = ros::Time::now();
+  }
 }
 
 int main(int argc, char **argv) {
